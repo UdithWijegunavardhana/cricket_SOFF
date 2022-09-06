e7dabb15cf8d64bf25eab631b6d9882c3e5b0ef8
ChethiyaNishanath <chethiyanishanath@gmail.com>
2022-09-06 17:15:23 +0530

    Added UI

--- lib/views/app/qr_code/qr_code_screen.dart
+++ lib/views/app/qr_code/qr_code_screen.dart
@@ -1,4 +1,6 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:soff_cricket_hybrid/services/auth/keycloak_auth_service.dart';
 
 class QRCodeScreen extends StatelessWidget {
   const QRCodeScreen({Key? key}) : super(key: key);
@@ -7,12 +9,20 @@ class QRCodeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.blueAccent,
-      child: const Center(
-        child: Text("Qr Code",
-        style: TextStyle(
-          color: Colors.white
-        ),
-        ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text("Qr Code",
+          style: TextStyle(
+            color: Colors.white
+          ),
+          ),
+          ElevatedButton(onPressed: () async {
+            context.router.popUntilRoot();
+            KeyCloakAuthService.login();
+          },
+              child: Text("Log Out"))
+        ],
       ),
     );
   }
