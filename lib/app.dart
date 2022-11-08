517877042f67f1699e6fbf8706e45135cf0e8599
tharakasandun <tharakesandun96@gmail.com>
2022-11-08 18:51:56 +0530

    ios release

--- lib/app.dart
+++ lib/app.dart
@@ -12,22 +12,21 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-
   final _appRouter = AppRouter(authGuard: AuthGuard());
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      return OrientationBuilder(
-          builder: (context, orientation) {
-            return GetMaterialApp.router(
-              initialBinding: ServiceBindings(),
-              routerDelegate: _appRouter.delegate(),
-              routeInformationProvider: _appRouter.routeInfoProvider(),
-              routeInformationParser: _appRouter.defaultRouteParser(),
-              builder: (context, router) => router!,
-            );
-          });
+      return OrientationBuilder(builder: (context, orientation) {
+        return GetMaterialApp.router(
+          initialBinding: ServiceBindings(),
+          routerDelegate: _appRouter.delegate(),
+          debugShowCheckedModeBanner: false,
+          routeInformationProvider: _appRouter.routeInfoProvider(),
+          routeInformationParser: _appRouter.defaultRouteParser(),
+          builder: (context, router) => router!,
+        );
+      });
     });
   }
 }
